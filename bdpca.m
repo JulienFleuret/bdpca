812139c2afd86e19dd772e233b8b7f8b145b8714
JSharp4273 <julien.fleuret@gmail.com>
2017-10-22 11:29:24 -0400

    Update bdpca.m

--- bdpca.m
+++ bdpca.m
@@ -1,4 +1,4 @@
-%Copyright 2017 Julien, Richard FLEURET
+%Copyright 2017 Julien FLEURET
 %
 %Redistribution and use in source and binary forms, with or without modification,
 % are permitted provided that the following conditions are met:
